4cc3206cedf83cc1fad28157877cb3689308bc90
alabri0803 <rashid92759346@gmail.com>
2025-06-16 17:01:28 +0000

    المرحلة 5

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import RentalContract
+from django.utils.html import format_html
 
-# Register your models here.
+
+@admin.register(RentalContract)
+class RentalContractAdmin(admin.ModelAdmin):
+  list_display = ('name', 'property_type', 'monthly_rent', 'start_date', 'end_date', 'total_fee', 'status_display')
+  list_filter = ('property_type',)
+
+  def status_display(self, obj):
+    color = {
+      'active': 'green',
+      'near_expiration': 'orange',
+      'expired': 'red',
+    }[obj.status]
+    return format_html('<span style="color: {};">{}</span>', color, obj.status)
+  status_display.short_description = 'الحالة'
--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from datetime import date
 
-# Create your models here.
+class PropertyType(models.TextChoices):
+  COMMERCIAL = 'commercial', _('محل تجاري')
+  RESIDENTIAL = 'residential', _('سكن')
+  COMPANY = 'company', _('شركة')
+
+class RentalContract(models.Model):
+  name = models.CharField(max_length=100, verbose_name=_('اسم المستأجر'))
+  property_type = models.CharField(max_length=20, choices=PropertyType.choices, verbose_name=_('نوع العقار'))
+  monthly_rent = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('الايجار الشهري'))
+  start_date = models.DateField(verbose_name=_('تاريخ بداية العقد'))
+  end_date = models.DateField(verbose_name=_('تاريخ نهاية العقد'))
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  @property
+  def total_fee(self):
+    return (self.monthly_rent * 12 * 0.03) + 5 + 1
+
+  @property
+  def days_remaining(self):
+    return (self.end_date - date.today()).days
+
+  @property
+  def status(self):
+    if self.days_remaining > 0:
+      return 'expired'
+    elif self.days_remaining <= 30:
+      return 'near_expiration'
+    return 'active'
+
+  def __str__(self):
+    return f"{self.name} - {self.get_property_type_display()}"
